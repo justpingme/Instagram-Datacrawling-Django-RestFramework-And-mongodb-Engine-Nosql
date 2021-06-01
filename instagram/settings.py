16644ac69e0e639929938f1b94278696ad71ca14
Abhinay raj <31544581+abhinayraj1226@users.noreply.github.com>
2021-06-01 15:02:00 +0530

    Update settings.py

--- instagram/settings.py
+++ instagram/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tdmp)$b)4_dp#=9$+5z$c*6pekp5&j!#ftzjd(a0+z!h#96xto'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
